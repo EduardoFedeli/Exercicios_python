81ca72aae9a02b38567dc6f99bba304666e7fda4
EduardoFedeli <dudufedeli@outlook.com>
2025-08-26 17:29:50 -0300

    first commit

---
+++ Funções.py
@@ -0,0 +1,19 @@
+# #Exercicio 5
+# lista = [3, 7, 10, 11, 2, 9]
+# soma = 0
+
+# for i in lista:
+#     if i % 2 == 1 and i > 5:
+#         soma = soma + i
+
+# print(soma)
+
+#Exercício 6
+# fruta = 'banana'
+# contador = 0
+
+# for a in fruta:
+#      contador = fruta.count(a)
+
+# print(contador)
+
